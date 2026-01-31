6ee17959f95eb0d010d478e0bd608faa5d19c84e
Sai-Sampath-Vegi <saisampathvegi@gmail.com>
2026-01-31 12:39:10 +0530

    Add files via upload

---
+++ StartUbuntuGUI.sh
@@ -0,0 +1,33 @@
+launch_rdp() {
+	/mnt/c/Windows/System32/mstsc.exe /v:localhost:3390
+}
+
+get_xrdp_status() {
+	systemctl is-active --quiet xrdp
+}
+
+sudo pkill -u ubuntu
+
+if ! get_xrdp_status; then
+
+	sudo systemctl start xrdp
+	echo Started Ubuntu GUI
+
+	launch_rdp
+else
+
+	echo Ubuntu GUI already Running
+
+	read -p "Enter Yes to reconnect to the GUI: " confirmation
+
+	if grep -iq "yes" <<< "$confirmation"; then
+		launch_rdp
+		echo Reconnecting to Ubuntu GUI
+	fi
+fi
+
+if ! get_xrdp_status; then
+	echo Stopped Ubuntu GUI
+else
+	echo Disconnected from Ubuntu GUI
+fi
---
+++ StopUbuntuGUI.sh
@@ -0,0 +1,8 @@
+if systemctl is-active --quiet xrdp; then
+	sudo systemctl stop xrdp
+	echo Stopped Ubuntu GUI
+else
+	echo Ubuntu GUI is not Running
+fi
+
+sudo pkill -u ubuntu
